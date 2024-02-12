a52fc1004d6fad828bf57053f348eef61e38fa7f
Mahendra kundare <>
2024-02-12 12:22:48 +0530

    Reading configurations using @Value annotation

---
+++ section6/v1-springboot/accounts/src/main/java/com/mk/accounts/exception/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package com.mk.accounts.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param resourceName the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     * @param fieldName the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     * @param fieldValue the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with the given input data %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
