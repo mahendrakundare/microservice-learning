5f8a0f255e73753cfd344cf46dd327848c479858
Mahendra kundare <>
2024-02-20 22:20:11 +0530

    Implementing Tracing using Grafana, Tempo & OpenTelemetry

--- section_11/accounts/src/main/java/com/mk/accounts/controller/CustomerController.java
+++ section_11/accounts/src/main/java/com/mk/accounts/controller/CustomerController.java
@@ -61,8 +61,9 @@ public class CustomerController {
     public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("mkbank-correlation-id") String correlationId,
                                                                    @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("mk-bank-correlation-id found {}", correlationId);
+        logger.debug("fetchCustomerDetails method start");
         CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.ok(customerDetailsDto);
     }
 }
