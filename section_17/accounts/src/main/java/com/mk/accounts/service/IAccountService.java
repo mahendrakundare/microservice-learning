c398fa11ba1cbb1ce566f6ca66d9b65745ebc8ff
Mahendra kundare <>
2024-02-27 18:03:38 +0530

    added server side loadbalancing using k8s discovery

---
+++ section_17/accounts/src/main/java/com/mk/accounts/service/IAccountService.java
@@ -0,0 +1,41 @@
+package com.mk.accounts.service;
+
+import com.mk.accounts.dto.CustomerDto;
+
+public interface IAccountService {
+
+    /**
+     *
+     * @param customerDto - CustomerDto object
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Account Details based on a given mobile number
+     */
+    CustomerDto fetchAccountDetails(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
+
+    /**
+     *
+     * @param accountNumber
+     * @return boolean indicating if the update of communication status is successful or not
+     */
+    boolean updateCommunicationStatus(Long accountNumber);
+
+}
